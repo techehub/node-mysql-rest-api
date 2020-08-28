2942506af44ef188eccbca41c61c638ed7a5a8da
vijeesh <vijeesh.tp@expertzlab.com>
2020-08-28 14:02:11 +0530

    sql

---
+++ script.sql
@@ -0,0 +1,8 @@
+create table user (
+    id int not null auto_increment ,
+    fname varchar (100) not null,
+    lname varchar (100) ,
+    email varchar (100),
+    phone varchar (15),
+    primary key (id)
+)
